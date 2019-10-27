a2499d126e019100fae2d78f9f80a1fcd68effae
Don Alvarez <don@acceleratedpictures.com>
2019-10-27 11:26:04 -0700

    Remove unused article interface

--- src/article/article.interface.ts
+++ src/article/article.interface.ts
@@ -1,20 +1,7 @@
-import { UserData } from '../user/user.interface';
 import { ArticleEntity } from './article.entity';
+
 interface Comment {
   body: string;
-}
-
-interface ArticleData {
-  slug: string;
-  title: string;
-  description: string;
-  body?: string;
-  tagList?: string[];
-  createdAt?: Date
-  updatedAt?: Date
-  favorited?: boolean;
-  favoritesCount?: number;
-  author?: UserData;
 }
 
 export interface CommentsRO {
